c072969b68397669c128436e9508e8f1d5a0bc9f
김서영 <bestseo02@naver.com>
2020-01-20 16:37:31 +0900

    solved

--- cpp/boj2225.cpp
+++ cpp/boj2225.cpp
@@ -2,8 +2,8 @@
 #include <string.h>
 using namespace std;
 
-long long dp(const int N, const int K){
-    long long _sum[K+1][N+2];
+int dp(const int N, const int K){
+    int _sum[K+1][N+2];
 
     for (int k(1); k < K+1; k++){
         for (int n(0); n < N+1; n++){
@@ -15,14 +15,12 @@ long long dp(const int N, const int K){
         for (int n(0); n < N+1; n++){
             if(k == 1 | n == 0) _sum[k][n] = 1;
             else {
-                for (int j(0); j < n+1; j++){
-                    _sum[k][n] += (_sum[1][j] * _sum[k-1][n-j]) % 1000000000;
-                }
+                _sum[k][n] = (_sum[k-1][n]+_sum[k][n-1])%1000000000;
             }
         }
     }
 
-    return _sum[K][N] % 1000000000;
+    return _sum[K][N];
 }
 
 int main(){
