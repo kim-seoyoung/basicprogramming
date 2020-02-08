602369b5a4e13a162b7407db0f4bf296503dca4b
김서영 <bestseo02@naver.com>
2020-02-08 11:38:02 +0900

    solved

---
+++ cpp/boj14501.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int N, schedule[32];
+int _max=0;
+
+void max_profit(int d, int lm){
+    for(int i(d); i < N; i++){
+        int dd=i+schedule[2*i];
+        if(dd < N){
+            // cout << d << " " << lm+schedule[2*i+1] << " \n";
+            max_profit(dd,lm+schedule[2*i+1]);
+        }
+        if(dd == N) _max = max(_max,lm+schedule[2*i+1]);
+        else _max = max(_max,lm);
+    }
+}
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    cin >> N;
+    for(int n(0); n < 2*N; n++){
+        cin >> schedule[n];
+    }
+
+    max_profit(0,0);
+
+    cout << _max;
+    
+    fclose(stdin);
+    return 0;
+}
